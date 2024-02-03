c9f64ac4fd2c08e6a747f5be82f817abeaa39e94
Sheila Hanson <slhanson11@live.com>
2024-02-03 14:04:39 -0600

    added model child folder syntax

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,30 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
-
+const sequelize = require("../config/connection.js");
+//Create a new squelized model for category
 class Category extends Model {}
 
 Category.init(
+  //Define fields/columns on model
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
--- models/Product.js
+++ models/Product.js
@@ -1,22 +1,51 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const { DECIMAL } = require("sequelize");
+const { INTEGER } = require("sequelize");
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
 
-// set up fields and rules for Product model
+//Define fields/columns on model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    price: {
+      type: DECIMAL,
+      allowNull: false,
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+
+    category_id: {
+      type: INTEGER,
+      model: "Category",
+      key: "id",
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
