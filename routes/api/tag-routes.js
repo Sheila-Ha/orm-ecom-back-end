04d4d7e92de792bb83345451d355cb5fd22adac5
Sheila Hanson <slhanson11@live.com>
2024-02-04 15:33:11 -0600

    updating api routes, tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,15 +2,34 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// Async and await used to handle routes asynchronously
+// Sequelize is used to fetch all tags from DB
+router.get('/', async (req, res) => {
+  // find all tags include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ 
+        model: Product, through: ProductTag, as: 'products' 
+      }],
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+    throw err;
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+  // find a single tag by its `id' to include its associated Product data
+  try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through:ProductTag }],
+    });
+    
+    if (!tag) {
+      return res.status(404).json({err});
+    }
+  }
 });
 
 router.post('/', (req, res) => {
