5f298c4bbf5519c4f24ab682929b3519d1aab1be
Sheila Hanson <slhanson11@live.com>
2024-02-03 18:36:14 -0600

    add the rest of the get and post

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,13 +32,21 @@ router.get("/:id", async (req, res) => {
     }
     req.status(200).json(categoryData);
   } catch (err) {
+    // console.log(err);
     res.status(500).json(err);
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
-  
+  try {
+    const categoryData = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
