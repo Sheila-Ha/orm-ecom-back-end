a8e3b104497050535e1853e1dcf41a781754a177
Sheila Hanson <slhanson11@live.com>
2024-02-04 21:36:07 -0600

    updated server.js syntax - npm start is working and app is listening on port 3001

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const routes = require('./routes');
 // import sequelize connection
 const sequelize = require('./config/connection');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -14,6 +13,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: true }).then(() => {
-app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
